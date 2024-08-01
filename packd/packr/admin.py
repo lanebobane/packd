c8f8e8a065263ef6768778776e9723e6d1333dab
Lane Zimmerman <lanezimmerman@Lanes-MacBook-Pro.local>
2024-08-01 11:21:43 -0400

    modified admin columns

--- packd/packr/admin.py
+++ packd/packr/admin.py
@@ -8,5 +8,40 @@ admin.site.index_title = 'Packd Index Title Placeholder'
 
 
 # Register your models here.
-admin.site.register(Pack)
-admin.site.register(Item)
+
+@admin.register(Pack)
+class PackAdmin(admin.ModelAdmin):
+	list_display = ("pack_name", "traveler",)
+
+	def traveler(self, obj):
+		return obj.traveler.username
+
+	def pack_name(self, obj):
+		return obj.name
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+	list_display = ("item_name", "traveler", "weight", "dimension_x", "dimension_y", "dimension_z", "is_bag")
+
+
+	def item_name(self, obj):
+		return obj.name
+
+	def traveler(self, obj):
+		return item.travler.username
+
+	def weight(self, obj):
+		return obj.weight
+
+	def dimenson_x(self, obj):
+		return obj.dimenson_x
+
+	def dimenson_y(self, obj):
+		return obj.dimenson_y
+
+	def dimenson_z(self, obj):
+		return obj.dimenson_z
+
+	def is_bag(self, obj):
+		return obj.is_bag
