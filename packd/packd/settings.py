c2048313b14e0db42e70dd7a52195aabe7d4bc22
Lane Linux <lane.zimmerman@gmail.com>
2024-08-27 13:36:44 -0400

    m

--- packd/packd/settings.py
+++ packd/packd/settings.py
@@ -134,12 +134,7 @@ USE_TZ = True
 # ]
 
 
-STATIC_URL = "static/"
-<<<<<<< HEAD
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
-=======
->>>>>>> master
+STATIC_URL = "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
