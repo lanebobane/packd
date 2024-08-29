299ec51e7c324a8549ef676e99618c203dedb9e8
Lane Linux <lane.zimmerman@gmail.com>
2024-08-29 09:43:53 -0400

    added localhost to csrf trusted origins

--- packd/packd/settings.py
+++ packd/packd/settings.py
@@ -138,3 +138,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:1337'
+]
