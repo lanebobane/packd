e6a2d1b4467817c61e8f056d0a27f8ddeaf4a2c5
Lane Linux <lane.zimmerman@gmail.com>
2024-09-02 14:17:02 -0400

    addpack form style updated

--- packd/packr/forms.py
+++ packd/packr/forms.py
@@ -32,3 +32,11 @@ class PackForm(forms.ModelForm):
     class Meta:
         model = Pack
         fields = ["name", "bag", "items"]
+
+    def __init__(self, *args, **kwargs):
+        super(PackForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            if visible.field.widget.input_type == 'checkbox':
+                visible.field.widget.attrs['class'] = 'form-control-checkbox'
+            else:    
+                visible.field.widget.attrs['class'] = 'form-control'
