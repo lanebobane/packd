81999d18f4f1230a01377521112e68f14c662779
Lane Zimmerman <lane@Lanes-MacBook-Pro.local>
2024-10-08 09:09:44 -0400

    adopt button redirects to login if not logged in

--- packd/packr/views.py
+++ packd/packr/views.py
@@ -82,6 +82,8 @@ def share_pack(request, pk):
 
 
 def adopt_pack(request, pk):
+    if request.user.id is None: 
+        return redirect('/users/login')
     if request.method == "POST":
         pack = Pack.objects.get(pk=pk)
         adopted_pack = pack.adopt_pack(request.user.id)
